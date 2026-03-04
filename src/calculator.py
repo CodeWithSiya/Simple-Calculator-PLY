a5bca89ec5ffb53ac7d96dd5bbd9b1a6269a8c9b
Siyabonga Madondo <MDNSIY014@myuct.ac.za>
2026-03-04 16:35:32 +0200

    Enhance calculator parser to support variable assignments and improve expression handling

--- src/calculator.py
+++ src/calculator.py
@@ -45,13 +45,26 @@ def t_error(t):
 
 lexer = lex.lex()
 
+# Giving higher precedence to MULTIPLY and DIVIDE.
+precedence = (
+    ('left', 'PLUS', 'MINUS'),
+    ('left', 'MULTIPLY', 'DIVIDE')
+)
+
 # Defining productions for the parsing step.
 def p_calc(p):
     '''
     calc : expression
+         | var_assign
          | empty
     '''
-    print(p[1])
+    print(run(p[1]))
+
+def p_var_assign(p):
+    '''
+    var_assign : NAME EQUALS expression
+    '''
+    p[0] = (p[2], p[1], p[3])
 
 def p_expression(p):
     '''
@@ -69,18 +82,41 @@ def p_expression_int_float(p):
     '''
     p[0] = p[1]
 
+def p_expression_var(p):
+    '''
+    expression : NAME
+    '''
+    p[0] = ('var', p[1])
+
 def p_empty(p):
     '''
     empty :
     '''
     p[0] = None
 
+def p_error(p):
+    print("Syntax error found!")
+
+# Execute the calculator actions (recursive).
+def run(p):
+    if type(p) == tuple:
+        if p[0] == '+':
+            return run(p[1]) + run(p[2])
+        elif p[0] == '-':
+            return run(p[1]) - run(p[2])
+        elif p[0] == '*':
+            return run(p[1]) * run(p[2])
+        elif p[0] == '/':
+            return run(p[1]) * run(p[2])
+    else:
+        return p
+
 parser = yacc.yacc()
 
 print("Welcome to the PLY calculator!")
 while True:
     try:
-        s = input("Enter a valid expression:\n")
+        s = input(">> ")
     except EOFError:
         break
     parser.parse(s)
--- src/parsetab.py
+++ src/parsetab.py
@@ -6,9 +6,9 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'DIVIDE EQUALS FLOAT INT MINUS MULTIPLY NAME PLUS\n    calc : expression\n         | empty\n    \n    expression : expression DIVIDE expression\n               | expression MULTIPLY expression\n               | expression PLUS expression\n               | expression MINUS expression\n    \n    expression : INT\n               | FLOAT\n    \n    empty :\n    '
+_lr_signature = 'leftPLUSMINUSleftMULTIPLYDIVIDEDIVIDE EQUALS FLOAT INT MINUS MULTIPLY NAME PLUS\n    calc : expression\n         | var_assign\n         | empty\n    \n    var_assign : NAME EQUALS expression\n    \n    expression : expression MULTIPLY expression\n               | expression DIVIDE expression\n               | expression PLUS expression\n               | expression MINUS expression\n    \n    expression : INT\n               | FLOAT\n    \n    expression : NAME\n    \n    empty :\n    '
     
-_lr_action_items = {'INT':([0,6,7,8,9,],[4,4,4,4,4,]),'FLOAT':([0,6,7,8,9,],[5,5,5,5,5,]),'$end':([0,1,2,3,4,5,10,11,12,13,],[-9,0,-1,-2,-7,-8,-3,-4,-5,-6,]),'DIVIDE':([2,4,5,10,11,12,13,],[6,-7,-8,6,6,6,6,]),'MULTIPLY':([2,4,5,10,11,12,13,],[7,-7,-8,7,7,7,7,]),'PLUS':([2,4,5,10,11,12,13,],[8,-7,-8,8,8,8,8,]),'MINUS':([2,4,5,10,11,12,13,],[9,-7,-8,9,9,9,9,]),}
+_lr_action_items = {'INT':([0,8,9,10,11,12,],[5,5,5,5,5,5,]),'FLOAT':([0,8,9,10,11,12,],[6,6,6,6,6,6,]),'NAME':([0,8,9,10,11,12,],[7,14,14,14,14,14,]),'$end':([0,1,2,3,4,5,6,7,13,14,15,16,17,18,],[-12,0,-1,-2,-3,-9,-10,-11,-5,-11,-6,-7,-8,-4,]),'MULTIPLY':([2,5,6,7,13,14,15,16,17,18,],[8,-9,-10,-11,-5,-11,-6,8,8,8,]),'DIVIDE':([2,5,6,7,13,14,15,16,17,18,],[9,-9,-10,-11,-5,-11,-6,9,9,9,]),'PLUS':([2,5,6,7,13,14,15,16,17,18,],[10,-9,-10,-11,-5,-11,-6,-7,-8,10,]),'MINUS':([2,5,6,7,13,14,15,16,17,18,],[11,-9,-10,-11,-5,-11,-6,-7,-8,11,]),'EQUALS':([7,],[12,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -17,7 +17,7 @@ for _k, _v in _lr_action_items.items():
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
-_lr_goto_items = {'calc':([0,],[1,]),'expression':([0,6,7,8,9,],[2,10,11,12,13,]),'empty':([0,],[3,]),}
+_lr_goto_items = {'calc':([0,],[1,]),'expression':([0,8,9,10,11,12,],[2,13,15,16,17,18,]),'var_assign':([0,],[3,]),'empty':([0,],[4,]),}
 
 _lr_goto = {}
 for _k, _v in _lr_goto_items.items():
@@ -27,13 +27,16 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> calc","S'",1,None,None,None),
-  ('calc -> expression','calc',1,'p_calc','calculator.py',51),
-  ('calc -> empty','calc',1,'p_calc','calculator.py',52),
-  ('expression -> expression DIVIDE expression','expression',3,'p_expression','calculator.py',58),
-  ('expression -> expression MULTIPLY expression','expression',3,'p_expression','calculator.py',59),
-  ('expression -> expression PLUS expression','expression',3,'p_expression','calculator.py',60),
-  ('expression -> expression MINUS expression','expression',3,'p_expression','calculator.py',61),
-  ('expression -> INT','expression',1,'p_expression_int_float','calculator.py',67),
-  ('expression -> FLOAT','expression',1,'p_expression_int_float','calculator.py',68),
-  ('empty -> <empty>','empty',0,'p_empty','calculator.py',74),
+  ('calc -> expression','calc',1,'p_calc','calculator.py',57),
+  ('calc -> var_assign','calc',1,'p_calc','calculator.py',58),
+  ('calc -> empty','calc',1,'p_calc','calculator.py',59),
+  ('var_assign -> NAME EQUALS expression','var_assign',3,'p_var_assign','calculator.py',65),
+  ('expression -> expression MULTIPLY expression','expression',3,'p_expression','calculator.py',71),
+  ('expression -> expression DIVIDE expression','expression',3,'p_expression','calculator.py',72),
+  ('expression -> expression PLUS expression','expression',3,'p_expression','calculator.py',73),
+  ('expression -> expression MINUS expression','expression',3,'p_expression','calculator.py',74),
+  ('expression -> INT','expression',1,'p_expression_int_float','calculator.py',80),
+  ('expression -> FLOAT','expression',1,'p_expression_int_float','calculator.py',81),
+  ('expression -> NAME','expression',1,'p_expression_var','calculator.py',87),
+  ('empty -> <empty>','empty',0,'p_empty','calculator.py',93),
 ]
